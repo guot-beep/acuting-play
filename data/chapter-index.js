9ad609a2dba91834b31a05c40ef60d4ef31f4095
guot-beep <guotingru@gmail.com>
2026-08-31 23:39:25 -0700

    data: chapters.js now 14 chapters; regenerated chapter index

--- data/chapter-index.js
+++ data/chapter-index.js
@@ -17,5 +17,9 @@ window.AG_CHAPTER_INDEX = [
  {id:"07", title:"The Kiln Master&#39;s Dry Cough", titleZh:"窯場的乾咳", pattern:"Lung Yin Deficiency 肺陰虛"},
  {id:"08", title:"The Ferryman&#39;s Winter", titleZh:"船家的冬天", pattern:"Phlegm-Heat Obstructing the Lung 痰熱壅肺"},
  {id:"09", title:"The Tea Stall Keeper", titleZh:"茶攤的杜大娘", pattern:"Heart and Spleen Both Deficient 心脾兩虛"},
- {id:"10", title:"Old Wu, One Winter Later", titleZh:"吳伯，又一個冬天", pattern:"Kidney Yang Deficiency 腎陽虛"}
+ {id:"10", title:"Old Wu, One Winter Later", titleZh:"吳伯，又一個冬天", pattern:"Kidney Yang Deficiency 腎陽虛"},
+ {id:"11", title:"The Scribe Who Could Not Eat", titleZh:"寫不完的帳與吃不下的飯", pattern:"Liver invading the Spleen 肝脾不和"},
+ {id:"12", title:"The Weaver's Winter Cough", titleZh:"織坊的冬咳", pattern:"Lung and Spleen Qi Deficiency 肺脾氣虛"},
+ {id:"13", title:"The Dyer's Failing Sight", titleZh:"染坊師傅的眼睛", pattern:"Liver and Kidney Yin Deficiency 肝腎陰虛"},
+ {id:"14", title:"Fire Above, Dryness Below", titleZh:"上面是火，下面是乾", pattern:"Heart and Kidney Failing to Communicate 心腎不交"}
 ];
